16d6ac7647982da92454d0fd74088606ad7bab7a
Aryan <aupathak1234@gmail.com>
2024-12-04 21:50:55 +0530

    added day 4 part 2

--- day_4/day_4.dart
+++ day_4/day_4.dart
@@ -15,7 +15,7 @@ void main() {
     count += horizontalScan(chars.join());
     count += horizontalScan(chars.reversed.join());
   }
-  print(count);
+  // print(count);
   //positive diagonals
   int rows = lines.length;
   int cols = lines[0].length;
@@ -29,7 +29,7 @@ void main() {
         diagonal.add(lines[row][col]);
       }
     }
-    if(diagonal.length >= 4){
+    if (diagonal.length >= 4) {
       // print(diagonal.join());
       count += horizontalScan(diagonal.join());
       count += horizontalScan(diagonal.reversed.join());
@@ -45,13 +45,30 @@ void main() {
         diagonal.add(lines[row][col]);
       }
     }
-    if(diagonal.length >= 4){
+    if (diagonal.length >= 4) {
       // print(diagonal.join());
       count += horizontalScan(diagonal.join());
       count += horizontalScan(diagonal.reversed.join());
     }
   }
   print(count);
+
+  //part 2 from here
+
+  int count2 = 0;
+  for (int i = 0; i <= lines.length - 3; i++) {
+    for (int j = 0; j <= lines[0].length - 3; j++) {
+      List<String> selectedGrid = [];
+
+      for (int k = 0; k < 3; k++) {
+        selectedGrid.add(lines[i + k].substring(j, j + 3));
+      }
+
+      // print(selectedGrid);
+      count2 += gridScan(selectedGrid);
+    }
+  }
+  print(count2);
 }
 
 int horizontalScan(String line) {
@@ -59,6 +76,16 @@ int horizontalScan(String line) {
   for (int i = 0; i <= line.length - 4; i++) {
     if (line.substring(i, i + 4) == 'XMAS') {
       count += 1;
+    }
+  }
+  return count;
+}
+
+int gridScan(List<String> grid){
+  int count = 0;
+  if(((grid[0][0] == 'M' && grid[2][2] == 'S') || (grid[0][0] == 'S' && grid[2][2] == 'M')) && grid[1][1] == 'A'){
+    if((grid[0][2] == 'M' && grid[2][0] == 'S') || (grid[0][2] == 'S' && grid[2][0] == 'M')){
+      count+=1;
     }
   }
   return count;
